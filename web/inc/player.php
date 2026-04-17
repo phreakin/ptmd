6e9773bd314ddeb7e4d305639d33a18b66f17868
Phreakin <phreakin@gmail.com>
2026-04-16 21:40:04 -0700

    Create player.php

---
+++ web/inc/player.php
@@ -0,0 +1,15 @@
+<div class="video-container">
+    <video id="ptmdPlayer" controls>
+        <source src="episode.mp4" type="video/mp4">
+    </video>
+
+    <!-- Watermark -->
+    <img src="/assets/brand/watermarks/ptmd-watermark-white.png"
+         class="video-watermark">
+
+    <!-- Dynamic Overlay -->
+    <div id="overlayLayer" class="ptmd-overlay">
+        <img id="overlayImage" src="" class="ptmd-glow">
+        <div class="ptmd-grain"></div>
+    </div>
+</div>
